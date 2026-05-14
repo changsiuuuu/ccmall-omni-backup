f2011a82ad031e3ba9673c1d32db1339136522c5
hacksungboo <qngkrtjd@naver.com>
2026-05-14 17:11:34 +0900

    feat: CI/CD 기능 추가

--- infra/deployment/terraform/main.tf
+++ infra/deployment/terraform/main.tf
@@ -1,6 +1,8 @@
 # infra/deployment/terraform/main.tf
 # AWS에 Web, Rec 서버를 provisioning한다.
 
+# 2026.05.14 17:11 CI/CD 테스트용 주석 추가
+
 terraform {
   required_version = ">= 1.14.0, < 2.0.0"
   required_providers {
